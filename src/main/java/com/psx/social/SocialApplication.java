d36f2df8fd8f9086868209844409e5fd40e18af8
alienware <184500237@qq.com>
2021-02-07 10:13:15 +0800

    websocket bug fix

--- src/main/java/com/psx/social/SocialApplication.java
+++ src/main/java/com/psx/social/SocialApplication.java
@@ -4,17 +4,26 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableCaching
 @EnableWebSocket
 @MapperScan("com.psx.social.dao")
-public class SocialApplication {
+public class SocialApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SocialApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SocialApplication.class);
     }
 
 }
